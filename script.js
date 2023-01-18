941b50c0ab75cca0b89f26d7b1b2d8aa17da465c
Edyta Kawala <edyta.wer.kawala@gmail.com>
2023-01-18 17:50:34 +0100

    Fixed points counting issue. Game logic works properly.

--- script.js
+++ script.js
@@ -1,67 +1,76 @@
 let options = ["rock", "paper", "scissors"];
+let computerPoints = 0; 
+let userPoints = 0;
 
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random() * 3);
+    console.log(options[randomNumber]);
     return options[randomNumber];
 }
 
 function getUserChoice(){
     let userInput = prompt("Please choose between rock, paper and scissors.");
     userInput = userInput.toLowerCase();
-    if (!(userInput == "rock" || userInput == "paper" || userInput == "scissors")){
-        alert("Wrong input");
-    } else return userInput;    
+    return userInput;    
 } 
 
 function playRound(computerSelection, userSelection){
-    let computerPoints = 0; 
-    let userPoints = 0;
-    let summaryPoints = [computerPoints, userPoints];
     
     switch (computerSelection){
         case "rock":
             if (userSelection == "paper"){
-                alert("User wins this round!");
+                alert("You win this round!");
                 userPoints +=1;
             } else if(userSelection == "scissors"){
                 alert("Computer wins this round!");
                 computerPoints +=1;
-            } else alert("It is a tie this round!");
+            } else if(userSelection == "rock"){ 
+                alert("It is a tie this round!");
+            } else
+                computerPoints +=1; 
+                alert("Invalid input! Computer wins this round!");
             break;
         
         case "paper":
             if (userSelection == "scissors"){
-                alert("User wins this round!");
+                alert("You win this round!");
                 userPoints +=1;
             } else if(userSelection == "rock"){
                 alert("Computer wins this round!");
                 computerPoints +=1;
-            } else alert("It is a tie this round!");
+            } else if(userSelection == "paper"){ 
+                alert("It is a tie this round!");
+            } else
+                computerPoints +=1;
+                alert("Invalid input! Computer wins this round!");
             break;
 
         case "scissors":
             if (userSelection == "rock"){
-                alert("User wins this round!");
+                alert("You win this round!");
                 userPoints +=1;
             } else if(userSelection == "paper"){
                 alert("Computer wins this round!");
                 computerPoints +=1;
-            } else alert("It is a tie this round!");
+            } else if(userSelection == "scissors"){ 
+                alert("It is a tie this round!");
+            } else
+                computerPoints +=1;
+                alert("Invalid input! Computer wins this round!");
             break;
     }
-    return summaryPoints;
-    
+        
 }
 
 function gamePlay(){
     for (let i = 0; i <5; i++){
         playRound(getComputerChoice(), getUserChoice());
     }
-    if (playRound[0] > playRound[1]){
-        alert(`Computer wins! Score: ${playRound[0]} : ${playRound[1]}`);}
-    else {alert(`You win! Score: ${playRound[1]} : ${playRound[0]}`);}
-    
-    
-}
+    alert(`Computer: ${computerPoints} User: ${userPoints}`);
 
+    if (computerPoints > userPoints){
+        alert("Computer has won!");
+    } else alert("You have won!");
+
+}
 gamePlay();
