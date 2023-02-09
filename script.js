acd065ac43862dc29482d152b2f1a081e83c3add
Edyta Kawala <edyta.wer.kawala@gmail.com>
2023-02-09 22:13:02 +0100

    Create UI to the game.

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ let options = ["rock", "paper", "scissors"];
 let computerPoints = 0; 
 let userPoints = 0;
 
+const container = document.querySelector('.game-info');
+let score = document.createElement('h1');
+score.textContent = "";
+let roundInfo = document.createElement('p');
+roundInfo.textContent = "";
+container.appendChild(score);
+container.appendChild(roundInfo);
+
+
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random() * 3);
     console.log(options[randomNumber]);
@@ -9,50 +18,49 @@ function getComputerChoice(){
 }
 
 function playRound(computerSelection, userSelection){
-
     switch (computerSelection){
         case "rock":
             if (userSelection == "paper"){
-                console.log("You win this round!");
+                roundInfo.textContent = "You win this round!";
                 userPoints +=1;
             } else if(userSelection == "scissors"){
-                console.log("Computer wins this round!");
+                roundInfo.textContent = "Computer wins this round!";
                 computerPoints +=1;
             } else if(userSelection == "rock"){ 
-                console.log("It is a tie this round!");
+                roundInfo.textContent = "It is a tie this round!";
             } else if(userSelection != "rock" || userSelection != "paper" || userSelection != "scissors"){
                 computerPoints +=1; 
-                console.log("Invalid input! Computer wins this round!");
+                roundInfo.textContent = "Invalid input! Computer wins this round!";
             }
             break;
         
         case "paper":
             if (userSelection == "scissors"){
-                console.log("You win this round!");
+                roundInfo.textContent = "You win this round!";
                 userPoints +=1;
             } else if(userSelection == "rock"){
-                console.log("Computer wins this round!");
+                roundInfo.textContent =  "Computer wins this round!";
                 computerPoints +=1;
             } else if(userSelection == "paper"){ 
-                console.log("It is a tie this round!");
+                roundInfo.textContent = "It is a tie this round!";
             } else if(userSelection != "rock" || userSelection != "paper" || userSelection != "scissors"){
                 computerPoints +=1;
-                console.log("Invalid input! Computer wins this round!");
+                roundInfo.textContent = "Invalid input! Computer wins this round!";
             }
             break;
 
         case "scissors":
             if (userSelection == "rock"){
-                console.log("You win this round!");
+                roundInfo.textContent = "You win this round!";
                 userPoints +=1;
             } else if(userSelection == "paper"){
-                console.log("Computer wins this round!");
+                roundInfo.textContent = "Computer wins this round!";
                 computerPoints +=1;
             } else if(userSelection == "scissors"){ 
-                console.log("It is a tie this round!");
+                roundInfo.textContent = "It is a tie this round!";
             } else if(userSelection != "rock" || userSelection != "paper" || userSelection != "scissors"){
                 computerPoints +=1;
-                console.log("Invalid input! Computer wins this round!");
+                roundInfo.textContent = "Invalid input! Computer wins this round!";
             }
             break;
     }
@@ -60,18 +68,20 @@ function playRound(computerSelection, userSelection){
 }
 
 function gamePlay(){
-
     const choices = document.querySelectorAll('button');
     choices.forEach(choice => {
     choice.addEventListener('click',() => {
+        choice.classList.add('active');
+        setTimeout(() => {choice.classList.remove('active')}, 200);
         playRound(getComputerChoice(), choice.id);
+        score.textContent = (`Computer: ${computerPoints} User: ${userPoints}`);
         if (computerPoints == 5 || userPoints == 5){
-            console.log(`Computer: ${computerPoints} User: ${userPoints}`)
             if (computerPoints > userPoints){
-                console.log("Computer wins!");
-            } else (console.log("You win!"))
+                roundInfo.textContent = "COMPUTER WINS!";
+            } else (roundInfo.textContent = "YOU WIN!");
+            computerPoints = 0;
+            userPoints = 0;
         }
-        
         });
     });
 
